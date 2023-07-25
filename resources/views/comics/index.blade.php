d121d6f9ad9eff0e25e88d6a340ac3d5d3af04a1
marcobusacca <marcobusacca01@gmail.com>
2023-07-25 16:26:15 +0200

    [FEAT] index page layout

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,4 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container my-5">
+        <div class="row">
+            @forelse ($comics as $comic)
+                <div class="col-12 col-md-6 col-lg-4 my-5">
+                    <div class="card h-100">
+                        <!-- Card Image -->
+                        <img src="{{ Vite::asset('resources/img/')}}{{$comic->cover_image}}" alt="cover-image" class="card-img-top">
+                        <!-- Card Header -->
+                        <div class="card-header d-flex justify-content-center align-items-center text-center">
+                            <!-- Title -->
+                            <h2 class="fs-4 fw-bold py-2">{{ $comic->title }}</h2>
+                        </div>
+                        <!-- Card Body -->
+                        <div class="card-body">
+                            <!-- Series -->
+                            <div>
+                                <strong>Series:</strong>
+                                <span>{{ $comic->series }}</span>
+                            </div>
+                            <!-- Type -->
+                            <div>
+                                <strong>Type:</strong>
+                                <span>{{ $comic->type }}</span>
+                            </div>
+                            <!-- Price -->
+                            <div>
+                                <strong>Price:</strong>
+                                <span>{{ $comic->price }}</span>
+                            </div>
+                            <!-- Sale Date -->
+                            <div>
+                                <strong>Sale Date:</strong>
+                                <span>{{ $comic->sale_date }}</span>
+                            </div>
+                            <!-- More Info Button -->
+                            <div>
+                                <a href="{{ Route('index') }}" class="btn btn-primary">More Info</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @empty
+                <div class="col-12 col-md-6 col-lg-4">
+                    <h3>Comic not Found!</h3>
+                </div>
+            @endforelse
+        </div>
+    </div>
 @endsection
