5ebecdb75e7c4a89d50ec28ca9bec4c6ecec747c
marcobusacca <marcobusacca01@gmail.com>
2023-07-25 17:45:23 +0200

    Bonus Completato

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -9,18 +9,24 @@ use App\Models\Comic;
 class PageController extends Controller
 {
     public function index(){
+
         $comics = Comic::all();
+
         return view('comics.index', compact('comics'));
     }
 
     public function show($id){
+
         $comics = Comic::all();
+
         if ($id >= 0 && $id < count($comics)){
+
             $comic = $comics[$id];
+
             return view('comics.show', compact('comic'));
+
         } else{
             abort('404');
         }
     }
 }
-
